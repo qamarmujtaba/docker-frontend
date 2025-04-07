91fd33d3c2976a3d22f7bd7f4eebfde094dd4c5b
Ubuntu <ubuntu@ip-172-31-40-156.eu-north-1.compute.internal>
2025-04-07 11:27:15 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        // Change the URL to a relative path
+        fetch("/api")
+            .then((res) => res.text())
+            .then((data) => setMessage(data));
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Frontend is working!</h1>
+            <p>Backend says: {message}</p>
+        </div>
+    );
 }
 
 export default App;
+
