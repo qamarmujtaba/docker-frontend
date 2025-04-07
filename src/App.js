5b0fbfd12847dfb527ac463e3198ffe1c668d8b4
qamarmujtaba <qamarmujtaba9913@gmail.com>
2025-04-07 16:39:24 +0500

    new-edit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Frontend is working!</h1>
+            <h1>New Frontend is working!</h1>
             <p>Backend says: {message}</p>
         </div>
     );
